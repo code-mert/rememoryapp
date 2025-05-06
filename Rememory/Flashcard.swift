c4777bc4b9075d6d15990b9ce50420ffecb0b06e
Mert <mertakdemir9399@gmail.com>
2025-05-06 16:13:43 +0200

    Update Flashcard Model

--- Rememory/Flashcard.swift
+++ Rememory/Flashcard.swift
@@ -8,12 +8,14 @@ class Flashcard {
     var tags: [String]
     var createdAt: Date
     var imageData: Data?
+    var nextReviewDate: Date?
     
-    init(question: String, answer: String, tags: [String], createdAt: Date, imageData: Data? = nil) {
+    init(question: String, answer: String, tags: [String] = [], createdAt: Date = .now, imageData: Data? = nil, nextReviewDate: Date? = nil) {
         self.question = question
         self.answer = answer
         self.tags = tags
         self.createdAt = createdAt
         self.imageData = imageData
+        self.nextReviewDate = nextReviewDate
     }
 }
