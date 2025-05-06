9a8ffa0c5805381b93c36864002b99137d0f5727
Mert <mertakdemir9399@gmail.com>
2025-05-06 16:13:27 +0200

    Add Filter function in List View

--- Rememory/Views/FlashcardListView.swift
+++ Rememory/Views/FlashcardListView.swift
@@ -3,13 +3,19 @@ import SwiftData
 
 struct FlashcardListView: View {
     @Query var flashcards: [Flashcard]
+    @State private var searchText: String = ""
 
     var body: some View {
-        List(flashcards) { flashcard in
+        let filtered = flashcards.filter {
+            searchText.isEmpty || $0.question.localizedCaseInsensitiveContains(searchText)
+        }
+
+        List(filtered) { flashcard in
             NavigationLink(destination: FlashcardDetailView(flashcard: flashcard)) {
                 Text(flashcard.question)
             }
         }
         .navigationTitle("Alle Karteikarten")
+        .searchable(text: $searchText)
     }
 }
